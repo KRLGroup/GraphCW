c38254064ebb8f3f6368f0f130d31fac7dfcedd8
Michela Proietti <mproietti@diag.uniroma1.it>
2024-10-24 16:30:10 +0200

    update dockerfile

--- Dockerfile/install_torch_scatter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install torch-scatter with CUDA support
-pip install torch-scatter==2.0.9 -f https://data.pyg.org/whl/torch-2.2.0+cu123.html
-
-# Verify the installation
-python -c "import torch; import torch_scatter; print(torch.cuda.is_available())"
